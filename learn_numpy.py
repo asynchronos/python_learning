c291d0b7fcacd1cc7128cb5b2dff6a176728dd38
Prakit Si <4016687+asynchronos@users.noreply.github.com>
2020-12-16 18:03:15 +0700

    Ndarray demo

--- learn_numpy.py
+++ learn_numpy.py
@@ -1,1 +1,15 @@
-import numpy
+import numpy as np 
+a = np.array([1,2,3]) 
+print(a)
+
+# more than one dimensions 
+a = np.array([[1, 2], [3, 4]]) 
+print(f"more than one dimension: {a}")
+
+# minimum dimensions 
+a = np.array([1, 2, 3,4,5], ndmin = 2) 
+print(f"minimum dimensions: {a}")
+
+# dt:ype parameter 
+a = np.array([1, 2, 3], dtype = complex) 
+print(f"dtype parameter: {a}")
